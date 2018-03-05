ac00582d1fb7ae7e0ba60cce59903cb296c032c5
Bisht <rahulbisht140@gmail.com>
2018-03-05 13:55:29 +0530

    added project summary

---
+++ comment.js
@@ -0,0 +1,10 @@
+/*
+as an etl-developer, my responsibilities were to analyse & cleaning of data by developing rules as per the requirement to 
+create different data sets.
+
+i was responsible to crawl thru websites and collection required data by creating bot apps in vb6.0, c# & kapow. i had to analyse
+the requirement, code & maintain the database(sql server & ms-access) for the different application.
+
+
+
+*/
